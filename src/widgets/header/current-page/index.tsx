9761f38d37fd06c0b2a45eaea5ad7a466be7afbb
Semyon Katz <kavabunga@gmail.com>
2024-07-08 19:24:07 +0300

    refactor: :recycle: refactor current page component logic

--- src/widgets/header/current-page/index.tsx
+++ src/widgets/header/current-page/index.tsx
@@ -1,11 +1,17 @@
 'use client';
 
 import { usePathname } from 'next/navigation';
+
+import { infoPagesCategories, projectPagesCategories } from '@/shared/data';
+
 import { CurrentPageUi } from './ui';
 
 export function CurrentPage() {
-  const path = usePathname();
-  const pageName = path.split('/').slice(-1)[0];
+  const possiblePages = projectPagesCategories
+    .map(({ name }) => name)
+    .concat(infoPagesCategories.map(({ name }) => name));
+  const path = usePathname().split('/').slice(-1)[0];
+  const pageName = possiblePages.find((name) => name === path) ? path : null;
 
-  return <CurrentPageUi {...{ pageName }} />;
+  return pageName && <CurrentPageUi {...{ pageName }} />;
 }
