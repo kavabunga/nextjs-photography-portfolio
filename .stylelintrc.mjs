8b4fae9d04eb52d8aa85a175141434e6fbe3cf87
Semyon Katz <kavabunga@gmail.com>
2024-07-08 13:59:18 +0300

    build: :technologist: tweak stylilint

--- .stylelintrc.mjs
+++ .stylelintrc.mjs
@@ -1,16 +1,14 @@
 const styleLintConfig = {
-  extends: ['stylelint-config-standard', 'stylelint-config-standard-scss'],
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-standard-scss',
+    'stylelint-config-clean-order',
+  ],
   plugins: ['stylelint-prettier'],
   rules: {
     'prettier/prettier': true,
-    'selector-class-pattern': [
-      '^[a-z_][A-Za-z_]+$',
-      {
-        message:
-          'Expected custom property name to be camelCase or snake_case. PascalCase and snake-case are prohibited.',
-      },
-    ],
-    'media-feature-range-notation': 'prefix',
+    'selector-class-pattern': null,
+    'custom-property-pattern': null,
   },
 };
 
