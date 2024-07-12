13a3f84d1cfe0264f14f684e953fa0fcacab8ef6
Semyon Katz <kavabunga@gmail.com>
2024-07-13 01:53:48 +0300

    refactor: :recycle: remove uneffective suspense with loader from gallery widget

--- src/widgets/gallery/index.tsx
+++ src/widgets/gallery/index.tsx
@@ -1,11 +1,8 @@
 'use client';
 
-import { Suspense } from 'react';
-
 import { useViewMode } from '@/shared/providers';
 import type { IImageData } from '@/shared/types';
 
-import { GalleryLoaderContainerUi } from './ui/gallery-loader-container';
 import { GalleryWidgetUi } from './ui';
 
 interface IGalleryWidget {
@@ -18,9 +15,5 @@ export function GalleryWidget({ data, category }: IGalleryWidget) {
   const isGridOn = pagesGridModes?.[category] || false;
 
   // TODO: Add something for empty data
-  return (
-    <Suspense fallback={<GalleryLoaderContainerUi />}>
-      <GalleryWidgetUi {...{ data }} {...{ isGridOn }} path={category} />
-    </Suspense>
-  );
+  return <GalleryWidgetUi {...{ data }} {...{ isGridOn }} path={category} />;
 }
